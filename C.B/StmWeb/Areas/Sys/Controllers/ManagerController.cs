b7ce6d942147e9ce465ba3d67d70522ed9bc3084
colin <337810174@qq.com>
2018-12-02 15:47:24 +0800

    core init web

---
+++ C.B/StmWeb/Areas/Sys/Controllers/ManagerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StmWeb.Models;
+
+
+namespace StmWeb.Area.Sys.Controllers
+{
+     [Area("Sys")]
+    public class ManagerController : Controller
+    {
+        public IActionResult Index1()
+        {
+            return View();
+        }
+    }
+}
