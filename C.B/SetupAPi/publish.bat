0937afb5474f6f3d5260c3e896967373998371ac
dxl <duxinglong@yijiupi.cn>
2018-12-06 16:30:12 +0800

    auto publish

--- C.B/SetupAPi/publish.bat
+++ C.B/SetupAPi/publish.bat
@@ -1,11 +1,18 @@
 :: dotnet core auto publish
-::@echo off
+@echo off
 cls
 echo ####################################################################################################################
 echo #####
+set gitPath=D:\repository\mine\CC-Base\
 set rootPath=D:\repository\mine\CC-Base\C.B\StmWeb
 set publishPath=E:\release\stm
 set port=8090
+
+
+
+CD /d %gitPath%
+	git co core
+	git pull origin core
 
 CD /d %rootPath%
 	dotnet restore
