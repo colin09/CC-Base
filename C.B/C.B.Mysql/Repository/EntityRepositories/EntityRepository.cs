ec939b5d9ea0b7d6409bea6c2fb278278f43980f
dxl <duxinglong@yijiupi.cn>
2018-12-10 18:06:13 +0800

    userRepository

--- C.B/C.B.Mysql/Repository/EntityRepositories/EntityRepository.cs
+++ C.B/C.B.Mysql/Repository/EntityRepositories/EntityRepository.cs
@@ -2,8 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using C.B.MySql.Repository.BaseM;
 
-namespace C.B.MySql.Repository.BaseM
+namespace C.B.MySql.Repository.EntityRepositories
 {
     //public partial class EntityRepository : BaseRepository<Entity> { }
 
--- C.B/StmWeb/Areas/Sys/Controllers/DevController.cs
+++ C.B/StmWeb/Areas/Sys/Controllers/DevController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using C.B.Models.Data;
+using C.B.MySql.Repository.EntityRepositories;
+using C.B.MySql.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using StmWeb.Models;
@@ -11,15 +14,30 @@ namespace StmWeb.Area.Sys.Controllers {
     [Area ("Sys")]
     [Authorize (Roles = "develop")]
     public class DevController : Controller {
-        
+
+        private readonly UserInfoRepository _userRepository;
+
+        public DevController () {
+            _userRepository = new UserInfoRepository ();
+        }
+
         public IActionResult Index () {
             return View ();
         }
 
-
         public IActionResult SysUsers () {
             return View ();
         }
+
+        public IActionResult GetSysUsersByPage (Pager pager) {
+           var result = _userRepository.Where(m=>true);
+            return Json (result);
+        }
+        public IActionResult CreateSysUsers (UserInfo model) {
+           var result = _userRepository.Insert(model);
+            return  result>0 Base;
+        }
+
 
     }
 }
