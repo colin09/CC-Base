3dcbaeb3016058e2dc6f7cda54ca939531bfc7f4
colin <337810174@qq.com>
2018-12-12 19:53:15 +0800

    db insert

--- C.B/StmWeb/Areas/Sys/Controllers/DevController.cs
+++ C.B/StmWeb/Areas/Sys/Controllers/DevController.cs
@@ -12,54 +12,44 @@ using StmWeb.Models;
 using C.B.Models.Enums;
 using C.B.Common.helper;
 
-namespace StmWeb.Area.Sys.Controllers {
-    [Area ("Sys")]
-    [Authorize (Roles = "develop")]
-    public class DevController : Controller {
+namespace StmWeb.Area.Sys.Controllers
+{
+    [Area("Sys")]
+    [Authorize(Roles = "develop")]
+    public class DevController : Controller
+    {
 
         private readonly UserInfoRepository _userRepository;
 
-        public DevController () {
-            _userRepository = new UserInfoRepository ();
+        public DevController()
+        {
+            _userRepository = new UserInfoRepository();
         }
 
-        public IActionResult Index () {
-            return View ();
+        public IActionResult Index()
+        {
+            return View();
         }
 
-        public IActionResult SysUsers () {
-            return View ();
+        public IActionResult SysUsers()
+        {
+            return View();
         }
 
         [HttpGet]
-        public IActionResult GetSysUsersByPage (Pager pager) {
-           var result = _userRepository.Where(m=>true).ToList();
-            return Json (result);
+        public IActionResult GetSysUsersByPage(Pager pager)
+        {
+            var result = _userRepository.Where(m => true).ToList();
+            return Json(result);
         }
 
         //public IActionResult CreateSysUsers (string userName,string trueName,string department,int authType) {
         [HttpPost]
-        public IActionResult CreateSysUsers (UserInfo model) {
-            System.Console.WriteLine("CreateSysUsers ---> ");
-            /*var model = new UserInfo(){
-                UserName = userName,
-                Password= "123456",
-                TrueName= trueName,
-                Department= department,
-                State = 1,
-                AuthType = (UserAuthType)authType,
-            };*/
-            System.Console.WriteLine(model.ToJson());
+        public IActionResult CreateSysUsers([FromBody]UserInfo model)
+        {
+            model.Password = CryptoHelper.MD5Encrypt("123456");
             var result = _userRepository.Insert(model);
-            var result2 = _userRepository.Insert(new UserInfo(){
-                UserName = "a",
-                TrueName="a",
-                Department= "a",
-                State=1,
-                AuthType = UserAuthType.develop,
-            });
-            add();
-            return Json( result>0 ? BaseResponse.SuccessResponse():BaseResponse.ErrorResponse("添加失败。"));
+            return Json(result > 0 ? BaseResponse.SuccessResponse() : BaseResponse.ErrorResponse("添加失败。"));
         }
 
 
@@ -67,15 +57,16 @@ namespace StmWeb.Area.Sys.Controllers {
 
 
 
-    private void add (){
-        var noticeR = new NoticeRepository();
-        noticeR.Insert(new Notice(){});
+        private void add()
+        {
+            var noticeR = new NoticeRepository();
+            noticeR.Insert(new Notice() { });
 
-        var messageR = new MessageRepository();
-        messageR.Insert(new Message(){});
+            var messageR = new MessageRepository();
+            messageR.Insert(new Message() { });
 
 
-    }
+        }
 
 
 
--- C.B/StmWeb/Areas/Sys/Controllers/LoginController.cs
+++ C.B/StmWeb/Areas/Sys/Controllers/LoginController.cs
@@ -2,6 +2,7 @@
 using C.B.Models.Data;
 using C.B.Models.Enums;
 using C.B.MySql.Data;
+using C.B.MySql.Repository.EntityRepositories;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
@@ -13,6 +14,14 @@ namespace StmWeb.Area.Sys.Controllers
     [Area("Sys")]
     public class LoginController : Controller
     {
+        private readonly UserInfoRepository _userRepository;
+
+        public LoginController()
+        {
+            _userRepository = new UserInfoRepository();
+        }
+
+
         public IActionResult Index()
         {
             return View();
@@ -27,6 +36,9 @@ namespace StmWeb.Area.Sys.Controllers
         [HttpPost]
         public async Task<IActionResult> SignIn(string userName, string password)
         {
+            if (userName.IsEmpty() || password.IsEmpty())
+                return Json(BaseResponse.ErrorResponse("用户名或密码错误。"));
+
             var user = new UserInfo
             {
                 UserName = userName,
@@ -65,80 +77,80 @@ namespace StmWeb.Area.Sys.Controllers
 
 
 
-/* AuthenticationScheme
+        /* AuthenticationScheme
 
-    1.---------------------------------------------------------------------------------------
-        dotnet add package Microsoft.AspNetCore.Authentication.Cookies --version 2.0.0
+            1.---------------------------------------------------------------------------------------
+                dotnet add package Microsoft.AspNetCore.Authentication.Cookies --version 2.0.0
 
-    2.---------------------------------------------------------------------------------------
-        services.AddAuthentication(options =>
-        {
-            options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-        })
-        .AddCookie(options =>
-        {
-            // 在这里可以根据需要添加一些Cookie认证相关的配置，在本次示例中使用默认值就可以了。
-        });
+            2.---------------------------------------------------------------------------------------
+                services.AddAuthentication(options =>
+                {
+                    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                })
+                .AddCookie(options =>
+                {
+                    // 在这里可以根据需要添加一些Cookie认证相关的配置，在本次示例中使用默认值就可以了。
+                });
 
-    3.---------------------------------------------------------------------------------------
-        app.UseAuthentication();
+            3.---------------------------------------------------------------------------------------
+                app.UseAuthentication();
 
-    4.---------------------------------------------------------------------------------------
-        var claimIdentity = new ClaimsIdentity("Cookie");
-        // var claimIdentity = new ClaimsIdentity("Cookie",ClaimTypes.Name,ClaimTypes.Role);
-        claimIdentity.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()));
-        claimIdentity.AddClaim(new Claim(ClaimTypes.Name, user.Name));
-        claimIdentity.AddClaim(new Claim(ClaimTypes.Email, user.Email));
-        claimIdentity.AddClaim(new Claim(ClaimTypes.MobilePhone, user.PhoneNumber));
-        claimIdentity.AddClaim(new Claim(ClaimTypes.DateOfBirth, user.Birthday.ToString()));
+            4.---------------------------------------------------------------------------------------
+                var claimIdentity = new ClaimsIdentity("Cookie");
+                // var claimIdentity = new ClaimsIdentity("Cookie",ClaimTypes.Name,ClaimTypes.Role);
+                claimIdentity.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()));
+                claimIdentity.AddClaim(new Claim(ClaimTypes.Name, user.Name));
+                claimIdentity.AddClaim(new Claim(ClaimTypes.Email, user.Email));
+                claimIdentity.AddClaim(new Claim(ClaimTypes.MobilePhone, user.PhoneNumber));
+                claimIdentity.AddClaim(new Claim(ClaimTypes.DateOfBirth, user.Birthday.ToString()));
 
-        var claimsPrincipal = new ClaimsPrincipal(claimIdentity);
-        // 在上面注册AddAuthentication时，指定了默认的Scheme，在这里便可以不再指定Scheme。
-        await context.SignInAsync(claimsPrincipal);
+                var claimsPrincipal = new ClaimsPrincipal(claimIdentity);
+                // 在上面注册AddAuthentication时，指定了默认的Scheme，在这里便可以不再指定Scheme。
+                await context.SignInAsync(claimsPrincipal);
 
-        await HttpContext.SignInAsync("MyCookieAuthenticationScheme", principal, new AuthenticationProperties
-        {
-            // 持久保存
-            IsPersistent = true
-            // 指定过期时间
-            ExpiresUtc = DateTime.UtcNow.AddMinutes(20)
-        });
+                await HttpContext.SignInAsync("MyCookieAuthenticationScheme", principal, new AuthenticationProperties
+                {
+                    // 持久保存
+                    IsPersistent = true
+                    // 指定过期时间
+                    ExpiresUtc = DateTime.UtcNow.AddMinutes(20)
+                });
 
- */
-
-
-/* JwtClaimTypes
-
-    1.---------------------------------------------------------------------------------------
-        dotnet add package IdentityModel --version 2.12.0
-
-    2.---------------------------------------------------------------------------------------
-        services.AddAuthentication(options =>
-        {
-            options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-        })
-        .AddCookie(options =>
-        {
-            // 在这里可以根据需要添加一些Cookie认证相关的配置，在本次示例中使用默认值就可以了。
-        });
-
-    3.---------------------------------------------------------------------------------------
-        app.UseAuthentication();
-
-    4.---------------------------------------------------------------------------------------
-        var claimIdentity = new ClaimsIdentity("Cookie", JwtClaimTypes.Name, JwtClaimTypes.Role);
-        claimIdentity.AddClaim(new Claim(JwtClaimTypes.Id, user.Id.ToString()));
-        claimIdentity.AddClaim(new Claim(JwtClaimTypes.Name, user.Name));
-        claimIdentity.AddClaim(new Claim(JwtClaimTypes.Email, user.Email));
-        claimIdentity.AddClaim(new Claim(JwtClaimTypes.PhoneNumber, user.PhoneNumber));
-        claimIdentity.AddClaim(new Claim(JwtClaimTypes.BirthDate, user.Birthday.ToString()));
-
-        var claimsPrincipal = new ClaimsPrincipal(claimIdentity);
-        // 在上面注册AddAuthentication时，指定了默认的Scheme，在这里便可以不再指定Scheme。
-        await context.SignInAsync(claimsPrincipal);
+         */
 
 
- */
+        /* JwtClaimTypes
+
+            1.---------------------------------------------------------------------------------------
+                dotnet add package IdentityModel --version 2.12.0
+
+            2.---------------------------------------------------------------------------------------
+                services.AddAuthentication(options =>
+                {
+                    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                })
+                .AddCookie(options =>
+                {
+                    // 在这里可以根据需要添加一些Cookie认证相关的配置，在本次示例中使用默认值就可以了。
+                });
+
+            3.---------------------------------------------------------------------------------------
+                app.UseAuthentication();
+
+            4.---------------------------------------------------------------------------------------
+                var claimIdentity = new ClaimsIdentity("Cookie", JwtClaimTypes.Name, JwtClaimTypes.Role);
+                claimIdentity.AddClaim(new Claim(JwtClaimTypes.Id, user.Id.ToString()));
+                claimIdentity.AddClaim(new Claim(JwtClaimTypes.Name, user.Name));
+                claimIdentity.AddClaim(new Claim(JwtClaimTypes.Email, user.Email));
+                claimIdentity.AddClaim(new Claim(JwtClaimTypes.PhoneNumber, user.PhoneNumber));
+                claimIdentity.AddClaim(new Claim(JwtClaimTypes.BirthDate, user.Birthday.ToString()));
+
+                var claimsPrincipal = new ClaimsPrincipal(claimIdentity);
+                // 在上面注册AddAuthentication时，指定了默认的Scheme，在这里便可以不再指定Scheme。
+                await context.SignInAsync(claimsPrincipal);
+
+
+         */
 
 
 
