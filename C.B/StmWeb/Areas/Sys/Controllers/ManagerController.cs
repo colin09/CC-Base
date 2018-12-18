224a512e4297a138bf54d9102d7d5fc7149bd51e
colin <337810174@qq.com>
2018-12-18 21:50:05 +0800

    curUser

--- C.B/StmWeb/Areas/Sys/Controllers/ManagerController.cs
+++ C.B/StmWeb/Areas/Sys/Controllers/ManagerController.cs
@@ -4,35 +4,52 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using StmWeb.Models;
 
-namespace StmWeb.Area.Sys.Controllers {
-    [Area ("Sys")]
-    [Authorize (Roles = "develop,admin")]
-    public class ManagerController : Controller {
-        
-        public IActionResult Index () {
-            return View ();
+namespace StmWeb.Area.Sys.Controllers
+{
+    [Area("Sys")]
+    [Authorize(Roles = "develop,admin")]
+    public class ManagerController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public ManagerController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
         }
 
 
-        public IActionResult EditerBasic () {
-            return View ();
+        public async Task<IActionResult> Index()
+        {
+
+            var curUser = HttpContext.User;
+            ApplicationUser user = await _userManager.GetUserAsync(HttpContext.User);
+            return View();
         }
 
 
-        public IActionResult EditerStandard () {
-            return View ();
+        public IActionResult EditerBasic()
+        {
+            return View();
         }
 
 
-        public IActionResult EditerFull () {
-            return View ();
+        public IActionResult EditerStandard()
+        {
+            return View();
         }
 
-        public IActionResult VideoPlay () {
-            return View ();
+
+        public IActionResult EditerFull()
+        {
+            return View();
+        }
+
+        public IActionResult VideoPlay()
+        {
+            return View();
         }
 
     }
