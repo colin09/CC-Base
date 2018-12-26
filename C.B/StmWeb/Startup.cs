33ed09d4996ad290dc68dd8203a14b170045405f
dxl <duxinglong@yijiupi.cn>
2018-12-26 18:40:20 +0800

    verifycodeimg

--- C.B/StmWeb/Startup.cs
+++ C.B/StmWeb/Startup.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using C.B.Common.Mapping;
+using C.B.MySql.AutoMapperConfig;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -11,31 +14,23 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
-using AutoMapper;
-using C.B.Common.Mapping;
-using C.B.MySql.AutoMapperConfig;
 
-namespace StmWeb
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace StmWeb {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices (IServiceCollection services) {
             //添加认证Cookie信息
-            services.AddAuthentication(Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationDefaults.AuthenticationScheme)
-             .AddCookie(options =>
-             {
-                 options.LoginPath = new PathString("/Sys/Login/Index");
-                 options.AccessDeniedPath = new PathString("/denied");
-             });
+            services.AddAuthentication (Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie (options => {
+                    options.LoginPath = new PathString ("/Sys/Login/Index");
+                    options.AccessDeniedPath = new PathString ("/denied");
+                });
 
             /*
             services.Configure<CookiePolicyOptions>(options =>
@@ -51,49 +46,49 @@ namespace StmWeb
                 );
             });*/
 
+            services.AddDistributedMemoryCache ();
+            services.AddSession (options => {
+                // Set a short timeout for easy testing.
+                options.IdleTimeout = TimeSpan.FromMinutes (5);
+                options.Cookie.HttpOnly = true;
+            });
+
             //services.AddAutoMapper();
-            services.AddAutoMapper(cfg => cfg.AddProfile<EditorModelMapperConfig>());
+            services.AddAutoMapper (cfg => cfg.AddProfile<EditorModelMapperConfig> ());
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
+            services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_1);
 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+            } else {
+                app.UseExceptionHandler ("/Home/Error");
+                app.UseHsts ();
             }
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseStaticFiles(new StaticFileOptions()
-            {
-                FileProvider = new PhysicalFileProvider(
-                Path.Combine(Directory.GetCurrentDirectory(), @"SourcesFile")),
-                RequestPath = new PathString("/Sources"),
-                OnPrepareResponse = ctx =>
-                {
-                    ctx.Context.Response.Headers.Append("Cache-Control", "public,max-age=600");
-                }
+            app.UseHttpsRedirection ();
+            app.UseStaticFiles ();
+            app.UseStaticFiles (new StaticFileOptions () {
+                FileProvider = new PhysicalFileProvider (
+                        Path.Combine (Directory.GetCurrentDirectory (), @"SourcesFile")),
+                    RequestPath = new PathString ("/Sources"),
+                    OnPrepareResponse = ctx => {
+                        ctx.Context.Response.Headers.Append ("Cache-Control", "public,max-age=600");
+                    }
             });
-            app.UseCookiePolicy();
-            app.UseAuthentication();
+            app.UseCookiePolicy ();
+            app.UseAuthentication ();
             //Mappings.RegisterMappings();
+            app.UseSession ();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
+            app.UseMvc (routes => {
+                routes.MapRoute (
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
-                routes.MapRoute(
+                routes.MapRoute (
                     name: "manager",
                     template: "{area=Sys}/{controller=Home}/{action=Index}/{id?}");
             });
