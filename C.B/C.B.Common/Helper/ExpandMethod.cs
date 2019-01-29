4faf6965cb8b32c2229cfafe824e7912ff4f7f17
dxl <duxinglong@yijiupi.cn>
2019-01-29 15:29:23 +0800

    takeString

--- C.B/C.B.Common/Helper/ExpandMethod.cs
+++ C.B/C.B.Common/Helper/ExpandMethod.cs
@@ -2,71 +2,54 @@
 using System.Text;
 using Newtonsoft.Json;
 
-
-namespace C.B.Common.helper
-{
-    public static class ExpandMethod
-    {
-        public static string Frmt(this string str, params object[] args)
-        {
-            return string.Format(str, args);
+namespace C.B.Common.helper {
+    public static class ExpandMethod {
+        public static string Frmt (this string str, params object[] args) {
+            return string.Format (str, args);
         }
 
-        public static bool IsEmpty(this string str)
-        {
-            return string.IsNullOrWhiteSpace(str);
+        public static bool IsEmpty (this string str) {
+            return string.IsNullOrWhiteSpace (str);
         }
-        public static byte[] ToByte(this string str)
-        {
-            return Encoding.UTF8.GetBytes(str);
+        public static byte[] ToByte (this string str) {
+            return Encoding.UTF8.GetBytes (str);
         }
 
-        public static int? ToInt(this string str)
-        {
-            if (str.IsEmpty())
+        public static int? ToInt (this string str) {
+            if (str.IsEmpty ())
                 return null;
             var result = 0;
-            var flag = int.TryParse(str, out result);
+            var flag = int.TryParse (str, out result);
             if (flag)
                 return result;
             return null;
         }
-        public static string ToJson(this object obj)
-        {
-            return JsonConvert.SerializeObject(obj);
+        public static string ToJson (this object obj) {
+            return JsonConvert.SerializeObject (obj);
         }
 
-
-        public static T DesJson<T>(this string obj)
-        {
-            return JsonConvert.DeserializeObject<T>(obj);
+        public static T DesJson<T> (this string obj) {
+            return JsonConvert.DeserializeObject<T> (obj);
         }
 
-
-        public static string ToDate(this double oadate)
-        {
-            var date = DateTime.Parse("1899-12-30").AddMilliseconds(Math.Round(oadate * 24 * 60 * 60 * 1000));
-            return date.ToString("yyyy-MM-dd");
+        public static string ToDate (this double oadate) {
+            var date = DateTime.Parse ("1899-12-30").AddMilliseconds (Math.Round (oadate * 24 * 60 * 60 * 1000));
+            return date.ToString ("yyyy-MM-dd");
         }
-        public static string ToDate(this DateTime date)
-        {
+        public static string ToDate (this DateTime date) {
             if (date == null)
                 return "";
-            return date.ToString("yyyy-MM-dd");
+            return date.ToString ("yyyy-MM-dd");
         }
-        public static string ToDate(this DateTime? date)
-        {
+        public static string ToDate (this DateTime? date) {
             if (date == null)
                 return "";
-            return date.Value.ToString("yyyy-MM-dd");
+            return date.Value.ToString ("yyyy-MM-dd");
         }
 
-
-        public static string ToFileType(this string fileExt)
-        {
+        public static string ToFileType (this string fileExt) {
             var fileType = "doc";
-            switch (fileExt.ToLower())
-            {
+            switch (fileExt.ToLower ()) {
                 case ".bmp":
                 case ".png":
                 case ".gif":
@@ -86,18 +69,16 @@ namespace C.B.Common.helper
             return fileType;
         }
 
-
-        public static string TakeString(this string str, int length)
-        {
-            if (string.IsNullOrEmpty(str))
+        public static string TakeString (this string str, int length) {
+            if (string.IsNullOrEmpty (str))
                 return str;
-            var tem = System.Text.RegularExpressions.Regex.Replace(str, "<[^>]+>", "");
-            if (!string.IsNullOrEmpty(tem.Trim()) && tem.Length > length)
-                return tem.Substring(0, length);
+            var tem = System.Text.RegularExpressions.Regex.Replace (str, "<[^>]+>", "");
+            tem = System.Text.RegularExpressions.Regex.Replace (tem, @"&[^;]*;", "");
+            
+            if (!string.IsNullOrEmpty (tem.Trim ()) && tem.Length > length)
+                return tem.Substring (0, length);
             return tem;
         }
-
-
 
     }
 }
