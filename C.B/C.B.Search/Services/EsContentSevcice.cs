463a6f19a0de87ab968d4d2c5d4f6df362299b2c
dxl <duxinglong@yijiupi.cn>
2019-04-26 09:44:45 +0800

    ext

--- C.B/C.B.Search/Services/EsContentSevcice.cs
+++ C.B/C.B.Search/Services/EsContentSevcice.cs
@@ -19,7 +19,33 @@ namespace C.B.Search.Services {
                 .Index (indexName.ToLower ())
                 .From ((page.PageIndex - 1) * page.PageSize)
                 .Size (page.PageSize)
-                .Query (q => q.Match (m => m.Field (f => f.Title).Query (key))));
+                .Query (q =>
+
+                    q.Match (m => m.Field (f => f.Title).Query (key))
+                )
+            );
+            // System.Console.WriteLine (result.Documents.ToJson ());
+            System.Console.WriteLine ($"{result.Documents.Count()}/{result.Total} , in {result.Took}ms");
+        }
+
+        public void Search (string key, string id, Pager page) {
+            var indexName = $"{_indexPrefix}{typeof(EsContent).Name}";
+
+            var result = _client.Search<EsContent> (s => s
+                //.Index (Indices.All)
+                //.Index (IndexName.From<EsContent>())
+                .Index (indexName.ToLower ())
+                .From ((page.PageIndex - 1) * page.PageSize)
+                .Size (page.PageSize)
+                .Query (q => {
+
+                    var query = q.Match (m => m.Field (f => f.Title).Query (key));
+                    if (id.IsNotEmpty ())
+                        query = query && q.Term (m => m.Id, id);
+
+                    return query;
+                })
+            );
             // System.Console.WriteLine (result.Documents.ToJson ());
             System.Console.WriteLine ($"{result.Documents.Count()}/{result.Total} , in {result.Took}ms");
         }
